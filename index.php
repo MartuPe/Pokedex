f2eafcd72e0900d26378f86d9bea70e8ebd01bfe
paibass <joancho2012@hotmail.com.ar>
2024-04-28 15:26:33 -0300

    Changes by LorenaPaolaJuhal

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php
+<header>
+
+    <div class="search">
+        <form action="buscar.php" method="GET">
+            <input type="text" name="pokemon" placeholder="Buscar Pokémon">
+            <button type="submit">Buscar</button>
+        </form>
+
+</header>
