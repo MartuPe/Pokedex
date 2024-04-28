4797c770c860151370231fd76cb04c199dbe5370
paibass <joancho2012@hotmail.com.ar>
2024-04-28 15:31:28 -0300

    Change

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <header>
-
     <div class="search">
         <form action="buscar.php" method="GET">
             <input type="text" name="pokemon" placeholder="Buscar Pokémon">
             <button type="submit">Buscar</button>
         </form>
-
 </header>
+<main>
+    <h1>Hola</h1>
+</main>
